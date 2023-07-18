be5cff2531558c02ed717aeb3160a8aeee023acf
asfandyar <asfandyar@falconit.com.pk>
2023-07-18 16:44:45 +0500

    Fix issue

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React deploy
+          Learn React deploy22
         </a>
       </header>
     </div>
