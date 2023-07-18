eacfb76b4de8990c3529c8cd1e81fbb103eb9de1
Asfandyaar2 <85154016+Asfandyaar2@users.noreply.github.com>
2023-07-18 13:27:27 +0000

    Deploying to gh-pages from @ Asfandyaar2/gh-pages-app@26b8ae9485ea307bff9676f402ed008e4ceb5a45 🚀

---
+++ precache-manifest.514b62bedb3952b2fb2e2fe42016582c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "706738a2ee2f4119e87a",
+    "url": "/gh-pages-app/static/css/main.e07b8fdd.chunk.css"
+  },
+  {
+    "revision": "706738a2ee2f4119e87a",
+    "url": "/gh-pages-app/static/js/main.bc9a7a0f.chunk.js"
+  },
+  {
+    "revision": "3aaafa75683e721c1ce0",
+    "url": "/gh-pages-app/static/js/runtime~main.3bf6f456.js"
+  },
+  {
+    "revision": "0335c0a13fecfb151ee5",
+    "url": "/gh-pages-app/static/js/2.49cb00a6.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/gh-pages-app/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "7366319f999625a1516903079bab06fd",
+    "url": "/gh-pages-app/index.html"
+  }
+];
